3869b87e8663500c0ec224c3ceaad69cd48b3f48
tth37 <xgpsthd0902@outlook.com>
2022-01-14 17:28:59 +0800

    feat: posts module

---
+++ src/posts/posts.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreatePostDto } from './dto/create-post.dto';
+import { Post } from './entities/post.entity';
+
+@Injectable()
+export class PostsService {
+  constructor(
+    @InjectRepository(Post)
+    private postsRepository: Repository<Post>,
+  ) {}
+
+  findAll(): Promise<Post[]> {
+    return this.postsRepository.find();
+  }
+
+  findOne(id: string): Promise<Post> {
+    return this.postsRepository.findOne(id);
+  }
+
+  create(createPostDto: CreatePostDto) {
+    const post = this.postsRepository.create(createPostDto);
+    return this.postsRepository.save(post);
+  }
+}
